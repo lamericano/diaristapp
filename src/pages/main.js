c8890dc02673e1660837668ab4d5e6a4df01bf26
lamericano <luanamericano@gmail.com>
2019-05-05 18:57:56 -0300

    Update main.js

--- src/pages/main.js
+++ src/pages/main.js
@@ -7,7 +7,6 @@ import {
 	TouchableOpacity
   } from 'react-native';
 import { Container, Header, Content, Button, Text } from 'native-base';
-import { material } from 'react-native-typography'
 
 
 export default class Main extends Component {
